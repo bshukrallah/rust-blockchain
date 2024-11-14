471b7d699dd9a73395730855225fe184664b7655
Ben <bshukrallah@proton.me>
2024-11-14 18:44:09 -0500

    hashable

--- src/lib.rs
+++ src/lib.rs
@@ -80,8 +80,8 @@ pub fn difficulty_bytes_as_u128 (v: &Vec<u8>) -> u128 {
 
 mod block;
 pub use crate::block::Block;
-//mod hashable;
-//pub use crate::hashable::Hashable;
+mod hashable;
+pub use crate::hashable::Hashable;
 //mod blockchain;
 //pub use crate::blockchain::Blockchain;
 //pub mod transaction;
