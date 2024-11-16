0e0dfee0c4ce61ac1331c16ade4321c64ddb63e8
Ben <bshukrallah@proton.me>
2024-11-16 16:46:24 -0500

    added hasing

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@ use blockchainlib::*;
 
 fn main() {
 
-    let block = Block::new(0, current_time(), vec![0; 32], 0, "Genesis Block".to_owned());
-
+    let mut block = Block::new(0, current_time(), vec![0; 32], 0, "Genesis Block".to_owned());
 
     println!("{:?}", &block);
 
+    let h = block.hash();
+    println!("{:?}", &h);
+    block.hash = h;
+    println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("{:?}", &h);
 }
